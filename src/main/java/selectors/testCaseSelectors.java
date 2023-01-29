19e79f431d825d17824bb9b57eeb673da307c27e
Korbed <qakorbedewe@gmail.com>
2023-01-29 17:57:43 +0100

    New Selector

--- src/main/java/selectors/testCaseSelectors.java
+++ src/main/java/selectors/testCaseSelectors.java
@@ -5,5 +5,6 @@ import org.openqa.selenium.*;
 public class testCaseSelectors {
 
     public static By FIRST_CATEGORY_SELECTORS = By.xpath("//li[@class='product-category product first']/a");
+    public static By CHOOSE_PRODUCT = By.xpath();
 
 }
